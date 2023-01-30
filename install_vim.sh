d24b2ef25097ab929eab0b3e7e4c01e2aecf383b
Riccardo Salzer <116647578+das-salzamt@users.noreply.github.com>
2023-01-30 16:56:48 +0100

    Update install_vim.sh

--- install_vim.sh
+++ install_vim.sh
@@ -1,6 +1,13 @@
 sudo apt install silversearcher-ag
 
-cd ~/.vim/bundle
+# neovim
+mkdir -p ~/.config/nvim
+touch ~/.config/nvim/init.vim
+cp ~/.vimrc ~/.config/nvim/init.vim
+nvim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \\n       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cd ~/.vim/bundle
 git clone https://github.com/tpope/vim-rails.git
